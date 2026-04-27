96223f7a80bd7772edf6589ba2283bc6942b197b
Pechenkin <89127225193@mail.ru>
2026-04-27 14:16:51 +0300

    setting up the correct connection order

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
-//Settings EJS
-app.set('view engine', 'ejs');
-app.set('views', './2views');
-
-//Static files
-app.use(express.static('1public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+//parsers
+app.use(express.json()); //parse input JSON to JS-object (req.body)
+app.use(express.urlencoded({extended: true})); // parse input data form HTML-forms to object
 
 //Including routes
 const indexRouter = require('./5routes/index');
 app.use('/', indexRouter);
+
+//Static files
+app.use(express.static('1public')); // set route to static files
+
+//Settings EJS
+app.set('view engine', 'ejs');
+app.set('views', './2views');
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
