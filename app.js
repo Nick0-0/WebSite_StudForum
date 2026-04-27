c58c924e7385edfd4fab1158e63b4a3ddffb7ba5
Pechenkin <89127225193@mail.ru>
2026-04-27 19:09:14 +0300

    setting of app.js with comments

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-const port = process.env.PORT || 3000;
+//import necessary modules
+const express = require('express'); //main framework for develop web-project
+const logger = require('morgan'); //module for logging HTTP-requests
+const cors = require('cors'); //module for COR-requests proccessing
+const dotenv = require('dotenv'); //module for working with enviroment vars from .env
+const db = require('./db'); //connect to DB with local module
 
-//parsers
+//app initialization
+const app = express(); //creating an instance of Express-app
+const port = process.env.PORT || 3000; //defining the server launch port
+
+//setting of enviroment vars
+dotenv.config(); //loading of vars form .env to process.env
+
+//middlewares for data parsing
 app.use(express.json()); //parse input JSON to JS-object (req.body)
 app.use(express.urlencoded({extended: true})); // parse input data form HTML-forms to object
 
-//Including routes
-const indexRouter = require('./5routes/index');
-app.use('/', indexRouter);
+//setting of logging
+app.use(logger('dev')); //connecting of middleware logging in develop mode
 
 //Static files
 app.use(express.static('1public')); // set route to static files
 
 //Settings EJS
-app.set('view engine', 'ejs');
-app.set('views', './2views');
+app.set('view engine', 'ejs'); //set EJS as default template engine
+app.set('views', './2views'); //route to dir with EJS-templates
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+//Connecting routes
+const indexRouter = require('./5routes/index'); //import of main router
+app.use('/', indexRouter); //connecting routes from indexRouter to root path
+
+//exceptions catching
+app.use((err, req, res, next) => { //the global error handler
+    console.error('Error: ', err.stack); //write error to console
+    res.status(500).json({ //sending an error response
+        message: 'An internal server error has occured',
+        error: err.message
+    });
+});
+
+//connecting to DB
+db.connect() //connecting to DB
+    .then(() => {
+        console.log('Successful connect with DataBase');
+
+        //server start
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`);
+        });
+    })
+    .catch((error) => { //in the error case
+        console.log('Error of connect to the DB: ', error);
+        process.exit(1); //ending the process with an error code
+    });
+
+
+// additional processing of routes not found
+app.use((req, res) => {
+    res.status(404).send("Page not found");
 });
