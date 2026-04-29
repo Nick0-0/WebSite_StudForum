f31a262f0ecca439831636fbeba56ba73001e563
Pechenkin <89127225193@mail.ru>
2026-04-29 20:32:58 +0300

    deleted local error handler from api.js

--- 5routes/api.js
+++ 5routes/api.js
@@ -8,11 +8,5 @@ router.get('/documents/:id', documentController.getDocumentById);
 router.put('/documents/:id', documentController.updateDocument);
 router.delete('/documents/:id', documentController.deleteDocument);
 
-//404 error handler
-route.use((req, res, next) => {
-    const error = new Error('Invalid route');
-    error.status = 404;
-    next(error);
-});
 
 module.exports = router;
