248147f11f3ba518d8b9bf927e1bc80a58e7dcef
Pechenkin <89127225193@mail.ru>
2026-04-29 20:52:32 +0300

    added prefics to api and admin routes

--- 5routes/admin.js
+++ 5routes/admin.js
@@ -7,34 +7,34 @@ const forumController = require('../3controllers/forumController');
 
 //user management section
 //getting all users list
-router.get('/users', userController.getUsers);
+router.get('/admin/users', userController.getUsers);
 //getting info about user by id
-router.get('/users', userController.getUserById);
+router.get('/admin/users', userController.getUserById);
 //updating info about user
-router.put('/users/:id', userController.updateUser);
+router.put('/admin/users/:id', userController.updateUser);
 //deleting user
-router.delete('/users/:id', userController.deleteUser);
+router.delete('/admin/users/:id', userController.deleteUser);
 
 //documents management section
 //getting all docs list
-router.get('/documents', userController.getDocuments);
+router.get('/admin/documents', userController.getDocuments);
 //deleting doc by id
-router.delete('/documents/:id', userController.deleteDocument);
+router.delete('/admin/documents/:id', userController.deleteDocument);
 
 //events management section
 //getting all events list
-router.get('/events', calendarController.getEvents);
+router.get('/admin/events', calendarController.getEvents);
 //creating new event
-router.post('/events', calendarController.createEvent);
+router.post('/admin/events', calendarController.createEvent);
 //updating event by id
-router.put('/events/:id', calendarController.updateEvent);
+router.put('/admin/events/:id', calendarController.updateEvent);
 //deleting event by id
-router.delete('/events/:id', calendarController.deleteEvent);
+router.delete('/admin/events/:id', calendarController.deleteEvent);
 
 //getting all topics list
-router.get('/topics', forumController.getTopics);
+router.get('/admin/topics', forumController.getTopics);
 //deleting topic by id
-router.delete('/topics/:id', forumController.deleteTopic);
+router.delete('/admin/topics/:id', forumController.deleteTopic);
 
 
 //export router for app using
--- 5routes/api.js
+++ 5routes/api.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const documentController = require('../3controllers/documentController');
 
 //CRUD operations with documents
-router.get('/documents', documentController.getDocument);
-router.post('/documents', documentController.createDocument);
-router.get('/documents/:id', documentController.getDocumentById);
-router.put('/documents/:id', documentController.updateDocument);
-router.delete('/documents/:id', documentController.deleteDocument);
+router.get('/api/documents', documentController.getDocument);
+router.post('/api/documents', documentController.createDocument);
+router.get('/api/documents/:id', documentController.getDocumentById);
+router.put('/api/documents/:id', documentController.updateDocument);
+router.delete('/api/documents/:id', documentController.deleteDocument);
 
 
 module.exports = router;
