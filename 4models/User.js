c8c6800e415cd546748311fe1e9484763da8182d
Pechenkin <89127225193@mail.ru>
2026-04-29 23:14:22 +0300

    models Student and Admin class in User.js

--- 4models/User.js
+++ 4models/User.js
@@ -1,31 +1,123 @@
 const {Database} = require('../db');
 
-class User {
+class BaseUser {
     constructor(data) {
         this.id = data.id;
-        this.username = data.username;
+        this.login = data.login;
         this.password = data.password;
         this.role = data.role || 'user';
+        this.email = data.email;
     }
 
-    static async create(user) {
-        //logic of user creation
-        const db = new Database();
-        const result = await db.query(
-            'INSERT INTO users (username, password, role) VALUES (?, ?, ?)',
-            [user.username, user.password, user.role]
-        );
-        return result.insertId;
-    }
-
-    static async getByID(id) {
-        //Search user by ID
-        const db = new Database();
-        const result = await db.query(
-            'SELECT * FROM users WHERE id = ?',
-            [id]
-        );
+    async authenticate(password) {
+        return bcrypt.compare(password, this.password);
     }
 }
 
-module.exports = User;
+class Student extends BaseUser {
+    constructor(data) {
+        super(data);
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.course = data.course;
+        this.faculty = data.faculty;
+        this.groupNumber = data.groupNumber;
+        this.photo = data.photo;
+        this.description = data.description;
+    }
+
+    async create(user) {
+        try {
+            const result = await db.run(`
+                INSERT INTO Students (
+                login,
+                password,
+                first_name,
+                last_name,
+                email,
+                course,
+                faculty,
+                group_number,
+                photo,
+                description
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                `, [
+                    this.login,
+                    this.password,
+                    this.firstName,
+                    this.lastName,
+                    this.email,
+                    this.course,
+                    this.faculty,
+                    this.groupNumber,
+                    this.photo,
+                    this.description
+                ]);
+                this.id = result.lastID;
+                return this;
+        } catch(error) {
+            throw new Error('Create student error', {cause: error});
+        }
+    }
+
+    async findById(id) {
+        try {
+            const result = await db.query('SELECT * FROM Students WHERE id = ?', [id]);
+            return result.length > 0 ? new Student(result[0]) : null;
+        } catch(error) {
+            throw new Error('Get student error', {cause: error});
+        }
+    }
+}
+
+class Admin extends BaseUser {
+    constructor(data) {
+        super(data);
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.photo = data.photo;
+    }
+
+    async create(user) {
+        try {
+            const result = await db.run(`
+                INSERT INTO Admins (
+                login,
+                password,
+                first_name,
+                last_name,
+                email,
+                photo
+                ) VALUES (?, ?, ?, ?, ?, ?)
+                `, [
+                    this.login,
+                    this.password,
+                    this.firstName,
+                    this.lastName,
+                    this.email,
+                    this.course,
+                    this.faculty,
+                    this.groupNumber,
+                    this.photo,
+                    this.description
+                ]);
+                this.id = result.lastID;
+                return this;
+        } catch(error) {
+            throw new Error('Create admin error', {cause: error});
+        }
+    }
+
+    async findById(id) {
+        try {
+            const result = await db.query('SELECT * FROM Admins WHERE id = ?', [id]);
+            return result.length > 0 ? new Admin(result[0]) : null;
+        } catch(error) {
+            throw new Error('Get admin error', {cause: error});
+        }
+    }
+}
+
+module.exports = {
+    Student, Admin
+};
