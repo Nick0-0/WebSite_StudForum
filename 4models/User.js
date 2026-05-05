f0c23e0c64d0143d053af4cfdb7df215f77af348
Pechenkin <89127225193@mail.ru>
2026-05-06 01:49:25 +0300

    added to User-model missing funcs

--- 4models/User.js
+++ 4models/User.js
@@ -60,13 +60,100 @@ class Student extends BaseUser {
         }
     }
 
-    async findById(id) {
+    static async findById(id) {
         try {
             const result = await db.query('SELECT * FROM Students WHERE id = ?', [id]);
             return result.length > 0 ? new Student(result[0]) : null;
         } catch(error) {
-            throw new Error('Get student error', {cause: error});
+            throw new Error('Get student by id error', {cause: error});
         }
+    }
+
+    static async findByLogin(login) {
+        try {
+            const result = await db.query('SELECT * FROM Students WHERE login = ?', [login]);
+            return result.length > 0 ? new Student(result[0]) : null;
+        } catch (error) {
+            throw new Error('Get student by login error', {cause: error});
+        }
+    }
+
+    static async findByEmail(email) {
+        try {
+            const result = await db.query('SELECT * FROM Students WHERE email = ?', [email]);
+            return result.length > 0 ? new Student(result[0]) : null;
+        } catch (error) {
+            throw new Error('Get student by email error', {cause: error});
+        }
+    }
+
+    static async find() {
+        try {
+            const results = await db.query('SELECT * FROM Students');
+            return results.map(row => new Student(row));
+        } catch (error) {
+            throw new Error('General Get student error', {cause: error});
+        }
+    }
+
+    async update() {
+        try {
+            // role = ? role = student
+            await db.run(`
+                UPDATE Students SET
+                    login = ?,
+                    
+                    email = ?,
+                    first_name = ?,
+                    last_name = ?,
+                    course = ?,
+                    faculty = ?,
+                    group_number = ?,
+                    photo = ?,
+                    description = ?
+                WHERE id = ?
+                `, [
+                    this.login,
+                    // this.role,
+                    this.email,
+                    this.firstName,
+                    this.lastName,
+                    this.course,
+                    this.faculty,
+                    this.groupNumber,
+                    this.photo,
+                    this.description,
+                    this. id
+                ]);
+                return this;
+        } catch (error) {
+            throw new Error('Update student error', {cause: error});
+        }
+    }
+
+    async delete() {
+        try {
+            await db.run('DELETE FROM Students WHERE id = ?', [this.id]);
+            return true;
+        } catch (error) {
+            throw new Error('Delete user error', {cause: error});
+        }
+    }
+
+    toObject() {
+        return {
+            id: this.id,
+            login: this.login,
+            // role: this.role,
+            email: this.email,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            course: this.course,
+            faculty: this.faculty,
+            groupNumber: this.groupNumber,
+            photo: this.photo,
+            description: this.description
+        };
     }
 }
 
@@ -115,6 +202,86 @@ class Admin extends BaseUser {
         } catch(error) {
             throw new Error('Get admin error', {cause: error});
         }
+    }
+
+    //!!!PAUSED HERE!!!
+    static async findByLogin(login) {
+        try {
+            const result = await db.query('SELECT * FROM Admins WHERE login = ?', [login]);
+            return result.length > 0 ? new Admin(result[0]) : null;
+        } catch (error) {
+            throw new Error('Get admin by login error', {cause: error});
+        }
+    }
+
+    static async findByEmail(email) {
+        try {
+            const result = await db.query('SELECT * FROM Admins WHERE email = ?', [email]);
+            return result.length > 0 ? new Admin(result[0]) : null;
+        } catch (error) {
+            throw new Error('Get admin by email error', {cause: error});
+        }
+    }
+
+    static async find() {
+        try {
+            const results = await db.query('SELECT * FROM Admins');
+            return results.map(row => new Admin(row));
+        } catch (error) {
+            throw new Error('General Get admin error', {cause: error});
+        }
+    }
+
+    async update() {
+        try {
+            // role = ? role = student
+            await db.run(`
+                UPDATE Students SET
+                    login = ?,
+                    
+                    email = ?,
+                    first_name = ?,
+                    last_name = ?,
+                    photo = ?,
+                WHERE id = ?
+                `, [
+                    this.login,
+                    // this.role,
+                    this.email,
+                    this.firstName,
+                    this.lastName,
+                    this.photo,
+                    this.id
+                ]);
+                return this;
+        } catch (error) {
+            throw new Error('Update admin error', {cause: error});
+        }
+    }
+
+    async delete() {
+        try {
+            await db.run('DELETE FROM Admins WHERE id = ?', [this.id]);
+            return true;
+        } catch (error) {
+            throw new Error('Delete admin error', {cause: error});
+        }
+    }
+
+    toObject() {
+        return {
+            id: this.id,
+            login: this.login,
+            // role: this.role,
+            email: this.email,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            course: this.course,
+            faculty: this.faculty,
+            groupNumber: this.groupNumber,
+            photo: this.photo,
+            description: this.description
+        };
     }
 }
 
