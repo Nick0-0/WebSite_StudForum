f92c8571ae92826d8113b1bbf4ad282e981a2a5c
Pechenkin <89127225193@mail.ru>
2026-05-06 12:38:08 +0300

    final modified index.js by including main routes and connect routers

--- 5routes/index.js
+++ 5routes/index.js
@@ -2,16 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
-//middleware for authorization control
-// const {isAuthenticated} = require('../')
-//при добавлении проверки авторизации раскомментировать
+//include main routers
+const authRouter = require('./auth');
+const apiRouter = require('./api');
+const adminRouter = require('./admin');
+
+//basic routes
+router.use('/auth', authRouter); //router for autheticate
+router.use('/api', apiRouter);  //router for API requests
+router.use('/admin', adminRouter);
 
 //Routes
 router.get('/', (req, res) => {
     res.render('layout');
-});
-router.get('/auth', (req, res) => {
-    res.render('auth');
 });
 router.get('/calendar', (req, res) => {
     res.render('calendar');
@@ -31,6 +34,12 @@ router.use ((req, res, next) => {
     const error = new Error('Invalid route');
     error.status = 404;
     next(error);
+});
+
+//final error handler
+router.use ((error, req, res, next) => {
+    res.status(error.statuc || 500);
+    res.json({error: {message: error.message}});
 });
 
 module.exports = router;
