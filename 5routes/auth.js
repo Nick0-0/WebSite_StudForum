75124b7dd4f81cd0e690b29b03a34815ad477424
Pechenkin <89127225193@mail.ru>
2026-05-12 15:28:49 +0300

    Coded the auth.ejs

--- 5routes/auth.js
+++ 5routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../3controllers/userController');
 
-router.get('/auth', userController.renderAuth);
+router.get('/', userController.renderAuth);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/logout', userController.logout);
