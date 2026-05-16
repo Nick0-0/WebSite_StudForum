d1527ebc257775b1ec79dc281c6170d1a03b5739
Nick0-0 <89127225193@mail.ru>
2026-05-16 16:54:28 +0300

    modified auth/register form with valid input fields and small fix in layout

--- 1public/JS-scripts/main.js
+++ 1public/JS-scripts/main.js
@@ -267,4 +267,35 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
+    //---------------------------------------------------------
+    // AUTOMATIC MASK FOR TEL INPUT FIELD
+    //---------------------------------------------------------
+    const phoneInput = document.getElementById('phone-input');
+
+    if (phoneInput) {
+        phoneInput.addEventListener('input', (e) => {
+            let input = e.target.value.replace(/\D/g, '');
+
+            if (input.startsWith('7') || input.startsWith('8')) {
+                input = input.substring(1);
+            }
+
+            let formatted = '+7 ';
+            if (input.length > 0) {
+                formatted += '(' + input.substring(0, 3);
+            }
+            if (input.length >= 4) {
+                formatted += ') ' + input.substring(3, 6);
+            }
+            if (input.length >= 7) {
+                formatted += '-' + input.substring(6, 8);
+            }
+            if (input.length >= 9) {
+                formatted += '-' + input.substring(8, 10);
+            }
+
+            e.target.value = input.length === 0 ? '' : formatted;
+        });
+    }
 });
